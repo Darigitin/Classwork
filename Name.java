0a01a1dda89e56eeb5c12441c906f8197c0dbb78
Darigitin <Darigitin@hotmail.com>
2015-12-01 15:08:57 -0600

    Create Name.java

---
+++ Name.java
@@ -0,0 +1,15 @@
+//Name, Age, and Annual Income
+
+public class Name
+{
+	public static void main(String[] args)
+	{
+		String name = "Matt Vertefeuile";
+		int age = 20;
+		double annualPay = 1000000;
+		
+		System.out.println("My name is " + name + ", my age is " + age + " and I hope to earn $" + annualPay + " per year.");
+		
+	}
+	
+}
