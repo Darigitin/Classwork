70c14cc56533173909bce8f832470ebd7d75346a
Darigitin <Darigitin@hotmail.com>
2016-01-31 14:23:03 -0600

    Create Distance.java

---
+++ Classes-09/Programming-and-Logic-I/Lab04/Distance.java
@@ -0,0 +1,15 @@
+/Distance calculation
+
+public class Distance
+{
+        public static void main(String[] args)
+        {
+                int speed = 20;
+                int time = 10;
+                int distance;
+
+                distance = speed * time;
+
+                System.out.println("Distance equals " + distance);
+        }
+}
